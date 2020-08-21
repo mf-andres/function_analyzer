d619d4428516155dad9bab9888a9daf8e491858e
mfernandriu <munozfernandezandres@gmail.com>
2020-08-21 19:50:18 +0200

    feat: operation replacement successfully implemented

--- function_analyzer/tests/unitary/calculate_function_ordinate.py
+++ function_analyzer/tests/unitary/calculate_function_ordinate.py
@@ -1,5 +1,6 @@
 from function_analyzer.use_cases.calculate_function_ordinate import calculate_function_ordinate, do_addition, \
-    find_left_operand, find_left_sign_or_end_position, find_right_operand, find_right_sign_tail_or_end_position
+    find_left_operand, find_left_sign_or_end_position, find_right_operand, find_right_sign_tail_or_end_position, \
+    substitute_operation_for_partial_result
 
 
 def test_calculate_function_ordinate_for_two_addends_addition():
@@ -47,3 +48,17 @@ def test_find_right_sign_tail_or_end_position():
     returned_sign_tail_or_end_position = find_right_sign_tail_or_end_position(function_string, sign_position)
     expected_sign_tail_or_end_position = 3
     assert returned_sign_tail_or_end_position == expected_sign_tail_or_end_position
+
+
+def test_substitute_operation_for_partial_result():
+    function_string = '1+2'
+    left_operand_position = 0
+    right_operand_tail = 3
+    partial_result = 3
+    returned_function_string = substitute_operation_for_partial_result(function_string,
+                                                                       left_operand_position,
+                                                                       right_operand_tail,
+                                                                       partial_result)
+
+    expected_function_string = '3'
+    assert returned_function_string == expected_function_string
--- function_analyzer/use_cases/calculate_function_ordinate.py
+++ function_analyzer/use_cases/calculate_function_ordinate.py
@@ -23,12 +23,14 @@ def addition_found(function_string: str) -> bool:
 
 def do_addition(function_string: str) -> str:
     addition_sign_position = find_addition_sign_position(function_string)  # TODO different abstraction layers mixed
-    left_operand = find_left_operand(function_string, addition_sign_position)
-    right_operand = find_right_operand(function_string, addition_sign_position)
+    left_operand_position = find_left_operand_position(function_string, addition_sign_position)
+    right_operand_tail_position = find_right_operand_tail_position(function_string, addition_sign_position)
+    left_operand = find_left_operand(function_string, addition_sign_position, left_operand_position)
+    right_operand = find_right_operand(function_string, addition_sign_position, right_operand_tail_position)
     partial_result = float(left_operand) + float(right_operand)
     function_string = substitute_operation_for_partial_result(function_string,
-                                                              left_operand,
-                                                              right_operand,
+                                                              left_operand_position,
+                                                              right_operand_tail_position,
                                                               partial_result)
     function_string = do_operations(function_string)
     return function_string
@@ -39,14 +41,20 @@ def find_addition_sign_position(function_string: str) -> int:
     return addition_position
 
 
-def find_left_operand(function_string, sign_position) -> int:  # TODO duplicity
+def find_left_operand_position(function_string, sign_position):
     left_sign_or_end_position = find_left_sign_or_end_position(function_string, sign_position)
+    return left_sign_or_end_position
+
+def find_left_operand(function_string, sign_position, left_sign_or_end_position) -> int:  # TODO duplicity
     left_operand = function_string[left_sign_or_end_position:sign_position]
     return left_operand
 
 
-def find_right_operand(function_string, sign_position) -> int:
+def find_right_operand_tail_position(function_string, sign_position):
     right_sign_tail_or_end_position = find_right_sign_tail_or_end_position(function_string, sign_position)
+    return right_sign_tail_or_end_position
+
+def find_right_operand(function_string, sign_position, right_sign_tail_or_end_position) -> int:
     sign_tail_position = sign_position + 1
     right_operand = function_string[sign_tail_position:right_sign_tail_or_end_position]
     return right_operand
@@ -62,8 +70,8 @@ def find_right_sign_tail_or_end_position(function_string: str, sign_position: in
 
 def substitute_operation_for_partial_result(function_string,
                                             left_operand_position,
-                                            right_operand_position,
+                                            right_operand_tail,
                                             partial_result):
-    operation_to_be_replaced = function_string[left_operand_position, right_operand_position]
-    function_string = function_string.replace(operation_to_be_replaced, partial_result)
+    operation_to_be_replaced = function_string[left_operand_position:right_operand_tail]
+    function_string = function_string.replace(operation_to_be_replaced, str(partial_result))
     return function_string
