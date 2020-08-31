0e5899a26c57b1fb91b2cf52b62c5447386d2ac3
mfernandriu <munozfernandezandres@gmail.com>
2020-08-31 13:51:30 +0200

    feat: now Additions update right operation sign position after doing operation

--- function_analyzer/domain/operation/addition.py
+++ function_analyzer/domain/operation/addition.py
@@ -7,6 +7,7 @@ class Addition(Operation):
     def __init__(self, function_string, sign_position):
         self.function_string = function_string
         self.sign_position = sign_position
+        self.right_operation = None
 
     def do_operation(self):
         left_operand_position = find_left_operand_position(self.function_string, self.sign_position)
@@ -18,9 +19,17 @@ class Addition(Operation):
                                                                             left_operand_position,
                                                                             right_operand_tail_position,
                                                                             partial_result)
+        post_substitution_shift_length = len(left_operand) + len(right_operand)
+        self.right_operation.update(post_substitution_shift_length)
         return function_string
 
     @staticmethod
     def calculate_partial_result(left_operand, right_operand):
         partial_result = float(left_operand) + float(right_operand)
         return partial_result
+
+    def set_right_operation(self, right_operation):
+        self.right_operation = right_operation
+
+    def update(self, shift_lenght):
+        self.sign_position -= shift_lenght
