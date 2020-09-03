5a4bbef7be0f15b019bd854a0c99f5c995b6d3c7
mfernandriu <munozfernandezandres@gmail.com>
2020-09-03 21:14:35 +0200

    feat: now operations sorter sorts by priority then by position for many operands

--- function_analyzer/infrastracture/operation_sorter/operation_sorter.py
+++ function_analyzer/infrastracture/operation_sorter/operation_sorter.py
@@ -3,5 +3,5 @@ from function_analyzer.domain.operation.operation import Operation
 
 class OperationSorter:
     def sort_by_priority(self, operations: [Operation]):
-        operations = sorted(operations, key=lambda x: (x.sign_priority, x.sign_position), reverse=True)
+        operations = sorted(operations, key=lambda x: (x.sign_priority, x.sign_position))
         return operations
--- function_analyzer/tests/unitary/infrastructure/operation_sorter/test_operation_sorter.py
+++ function_analyzer/tests/unitary/infrastructure/operation_sorter/test_operation_sorter.py
@@ -2,12 +2,13 @@ from function_analyzer.infrastracture.operation_sorter.operation_sorter import O
 
 
 def test_sorts_two_operations_by_increasing_position():
-    class OperationMock:
-        def __init__(self, sign_position: int):
+    class OperationMock:  # TODO use fixtures
+        def __init__(self, sign_position: int, sign_priority: int):
             self.sign_position = sign_position
+            self.sign_priority = sign_priority
 
-    first_operation = OperationMock(1)
-    second_operation = OperationMock(5)
+    first_operation = OperationMock(1, 1)
+    second_operation = OperationMock(5, 1)
     operations = [first_operation, second_operation]
     operation_sorter = OperationSorter()
     sorted_operations = operation_sorter.sort_by_priority(operations)
@@ -16,17 +17,19 @@ def test_sorts_two_operations_by_increasing_position():
 
 def test_sorts_many_operations_by_increasing_position():
     class OperationMock:  # TODO use fixtures
-        def __init__(self, sign_position: int):
+        def __init__(self, sign_position: int, sign_priority: int):
             self.sign_position = sign_position
+            self.sign_priority = sign_priority
 
-    first_operation = OperationMock(1)
-    second_operation = OperationMock(2)
-    third_operation = OperationMock(3)
-    fourth_operation = OperationMock(4)
+    first_operation = OperationMock(1, 1)
+    second_operation = OperationMock(2, 1)
+    third_operation = OperationMock(3, 1)
+    fourth_operation = OperationMock(4, 1)
     operations = [fourth_operation, first_operation, third_operation, second_operation]
     operation_sorter = OperationSorter()
     sorted_operations = operation_sorter.sort_by_priority(operations)
-    assert sorted_operations[3] is fourth_operation
+    expected_sorted_operations = [first_operation, second_operation, third_operation, fourth_operation]
+    assert sorted_operations == expected_sorted_operations
 
 
 def test_sort_two_operations_by_priority_first_then_position():
@@ -35,9 +38,26 @@ def test_sort_two_operations_by_priority_first_then_position():
             self.sign_position = sign_position
             self.sign_priority = sign_priority
 
-    first_operation = OperationMock(2, 2)
-    second_operation = OperationMock(1, 1)
+    first_operation = OperationMock(2, 1)
+    second_operation = OperationMock(1, 2)
     operations = [second_operation, first_operation]
     operation_sorter = OperationSorter()
     sorted_operations = operation_sorter.sort_by_priority(operations)
     assert sorted_operations[0] is first_operation
+
+
+def test_sort_many_operations_by_priority_first_then_position():
+    class OperationMock:  # TODO use fixtures
+        def __init__(self, sign_position: int, sign_priority: int):
+            self.sign_position = sign_position
+            self.sign_priority = sign_priority
+
+    first_operation = OperationMock(1, 1)
+    second_operation = OperationMock(2, 1)
+    third_operation = OperationMock(3, 2)
+    fourth_operation = OperationMock(4, 2)
+    operations = [third_operation, fourth_operation, second_operation, first_operation]
+    operation_sorter = OperationSorter()
+    sorted_operations = operation_sorter.sort_by_priority(operations)
+    expected_sorted_operations = [first_operation, second_operation, third_operation, fourth_operation]
+    assert sorted_operations == expected_sorted_operations
