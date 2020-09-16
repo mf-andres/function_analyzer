183770ae50278ce0e5773c3790c68e16495da743
mfernandriu <munozfernandezandres@gmail.com>
2020-09-16 20:49:59 +0200

    test: added unitary test for Operation class

--- function_analyzer/tests/unitary/domain/test_operation.py
+++ function_analyzer/tests/unitary/domain/test_operation.py
@@ -2,25 +2,35 @@ from function_analyzer.domain.operand import Operand
 from function_analyzer.domain.operation import Operation
 
 
+# as Operand is just a value object it is not worthy of faking
+
+# TODO invert operand_finder dependency and unit test
+
+def test_calculate_shift_length():
+    left_operand = Operand(None, '123', None)
+    right_operand = Operand(None, '123', None)
+    partial_result = '123'
+    returned_shift_length = Operation.calculate_shift_length(left_operand, partial_result, right_operand)
+    expected_shift_length = 4
+    assert returned_shift_length == expected_shift_length
+
+
 def test_substitute_operands_and_operation_for_partial_result():
     function_string = '1+2'
-    sign_position = 1
-    operation = Operation(sign_position, 1)
 
     left_operand_position = 0
     left_operand_string = '1'
     left_operand_tail = 1
+    left_operand = Operand(left_operand_position, left_operand_string, left_operand_tail)
 
     right_operand_position = 2
     right_operand_string = '2'
     right_operand_tail = 3
+    right_operand = Operand(right_operand_position, right_operand_string, right_operand_tail)
 
     partial_result = "3"
 
-    left_operand = Operand(left_operand_position, left_operand_string, left_operand_tail)
-    right_operand = Operand(right_operand_position, right_operand_string, right_operand_tail)
-
-    returned_function_string = operation.substitute_operation_for_partial_result(
+    returned_function_string = Operation.substitute_operation_for_partial_result(
         function_string,
         left_operand,
         right_operand,
