4bdcb2e340e8d7f9585d116fa7ac8cae73bc5b33
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-10-24 12:38:27 +0200

    refactor: removed duplicities in test

--- function_analyzer/tests/integration/use_cases/test_calculate_function_ordinate.py
+++ function_analyzer/tests/integration/use_cases/test_calculate_function_ordinate.py
@@ -6,108 +6,104 @@ from function_analyzer.use_cases.calculate_function_ordinate import calculate_fu
 def test_calculate_function_ordinate_for_two_addends_addition():
     abscissa = 1
     function_string = 'x+1'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 2
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_many_addends_addition():
     abscissa = 1
     function_string = 'x+x+x+1'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 4
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_two_operands_substraction():
     abscissa = 1
     function_string = 'x-1'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 0
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_many_operands_substraction():
     abscissa = 1
     function_string = 'x-x-x-1'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = -2
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_two_operands_multiplication():
     abscissa = 2
     function_string = 'x*x'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 4
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_three_operands_multiplication_bug():
     abscissa = 2
     function_string = '8.0*2*1'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 16
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_many_operands_multiplication():
     abscissa = 2
     function_string = 'x*x*x*x*1'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 16
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_two_operands_division():  # TODO duplicities everywhere
     abscissa = 2
     function_string = 'x/x'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 1
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_many_operands_division():  # TODO duplicities everywhere
     abscissa = 2
     function_string = 'x/x/x/x'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 0.25
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_two_operands_exponentiation():  # TODO duplicities everywhere
     abscissa = 2
     function_string = 'xpx'
-    operation_finder = OperationFinder()
-    operation_sorter = OperationSorter()
-    returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
     expected_ordinate = 4
-    assert returned_ordinate == expected_ordinate
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
 
 
 def test_calculate_function_ordinate_for_many_operands_exponentiation():  # TODO duplicities everywhere
     abscissa = 2
     function_string = 'xpxpxpx'
+    expected_ordinate = 256
+    assert_solving_expression_returns_expected_ordinate(abscissa, function_string, expected_ordinate)
+
+
+# def test_calculate_function_ordinate_for_expression_with_multiplication_and_substraction():  # TODO duplicities everywhere
+#     abscissa = 2
+#     function_string = 'x*(x-x)'
+#     operation_finder = OperationFinder()
+#     operation_sorter = OperationSorter()
+#     returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
+#     expected_ordinate = 256
+#     assert returned_ordinate == expected_ordinate
+#
+#
+# def test_calculate_function_ordinate_for_many_expression_with_one_parenthesis():  # TODO duplicities everywhere
+#     abscissa = 2
+#     function_string = 'x*(x-x)'
+#     operation_finder = OperationFinder()
+#     operation_sorter = OperationSorter()
+#     returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
+#     expected_ordinate = 256
+#     assert returned_ordinate == expected_ordinate
+
+
+def assert_solving_expression_returns_expected_ordinate(
+        abscissa: float, function_string: str, expected_ordinate: float
+):
     operation_finder = OperationFinder()
     operation_sorter = OperationSorter()
     returned_ordinate = calculate_function_ordinate(operation_finder, operation_sorter, abscissa, function_string)
-    expected_ordinate = 256
     assert returned_ordinate == expected_ordinate
