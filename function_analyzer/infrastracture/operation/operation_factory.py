409ea091abc09f406f2a5f1b7a4ffe881b6b9b01
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-10-24 12:08:37 +0200

    feat: implemented division

--- function_analyzer/infrastracture/operation/operation_factory.py
+++ function_analyzer/infrastracture/operation/operation_factory.py
@@ -1,6 +1,7 @@
 from function_analyzer.domain.sign import Sign
 from function_analyzer.infrastracture.operand_finder.operand_finder import OperandFinder
 from function_analyzer.infrastracture.operation.addition import Addition
+from function_analyzer.infrastracture.operation.division import Division
 from function_analyzer.infrastracture.operation.substraction import Substraction
 from function_analyzer.infrastracture.operation.multiplication import Multiplication
 
@@ -9,6 +10,8 @@ class OperationFactory:
     @staticmethod
     def create(sign: str, sign_position: int):
         operand_finder = OperandFinder()
+        if sign == Sign.DIVISION_CHARACTER:
+            return Division(operand_finder, sign_position, Sign.DIVISION_PRIORITY)
         if sign == Sign.MULTIPLICATION_CHARACTER:
             return Multiplication(operand_finder, sign_position, Sign.MULTIPLICATION_PRIORITY)
         if sign == Sign.SUBSTRACTION_CHARACTER:
