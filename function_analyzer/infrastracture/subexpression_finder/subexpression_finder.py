ea5b92da2943f1796a9aa914ef5afc3b3352ac48
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-10-28 19:46:28 +0100

    test: subexpression finder finds subexpression with subexpressions

--- function_analyzer/infrastracture/subexpression_finder/subexpression_finder.py
+++ function_analyzer/infrastracture/subexpression_finder/subexpression_finder.py
@@ -8,30 +8,29 @@ from function_analyzer.infrastracture.subexpression_finder.subexpression_finder_
 class SubexpressionFinder(SubexpressionFinderInterface):
     @staticmethod
     def find_subexpressions(expression_string: str):
-        opening_parentheses = SubexpressionFinder.find_opening_parentheses(expression_string)
-        closing_parentheses = SubexpressionFinder.find_closing_parentheses(expression_string)
+        opening_parentheses, closing_parentheses = SubexpressionFinder.find_parentheses_pairs(expression_string)
 
         subexpressions = list()
         for opening_parenthesis, closing_parenthesis in zip(opening_parentheses, closing_parentheses):
             subexpression_string = expression_string[opening_parenthesis + 1: closing_parenthesis - 1]
-            subexpression = Expression(SubexpressionFinder(), OperationFinder(), OperationSorter(), subexpression_string)
+            subexpression = Expression(SubexpressionFinder(), OperationFinder(), OperationSorter(),
+                                       subexpression_string)
             subexpressions.append(subexpression)
 
         return subexpressions
 
     @staticmethod
-    def find_closing_parentheses(expression_string):
-        closing_parentheses = list()
-        for character_position, character in enumerate(expression_string):
-            if character == ")":
-                closing_parentheses.append(character_position)
-        return closing_parentheses
-
-    @staticmethod
-    def find_opening_parentheses(expression_string):
+    def find_parentheses_pairs(expression_string):
         opening_parentheses = list()
+        closing_parentheses = list()
+        parenthesis_yet_to_be_close = 0
         for character_position, character in enumerate(expression_string):
             if character == "(":
-                opening_parentheses.append(character_position)
-        return opening_parentheses
-
+                parenthesis_yet_to_be_close += 1
+                if parenthesis_yet_to_be_close == 1:
+                    opening_parentheses.append(character_position)
+            if character == ")":
+                parenthesis_yet_to_be_close -= 1
+                if parenthesis_yet_to_be_close == 0:
+                    closing_parentheses.append(character_position)
+        return opening_parentheses, closing_parentheses
