f4a9a414caba15efc487bc74eed71836aa22810e
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-10-28 19:21:42 +0100

    test: subexpression finder finds many subexpressions

--- function_analyzer/tests/integration/infrastructure/subexpression_finder/test_subexpression_finder.py
+++ function_analyzer/tests/integration/infrastructure/subexpression_finder/test_subexpression_finder.py
@@ -5,3 +5,9 @@ def test_finds_one_subexpression():  # TODO refactor things under here
     expression_string = "x*(x-1)"
     subexpressions = SubexpressionFinder.find_subexpressions(expression_string)
     assert len(subexpressions) == 1
+
+
+def test_finds_many_subexpressions():
+    expression_string = "x*(x-1)*(x+1)*(x+1)"
+    subexpressions = SubexpressionFinder.find_subexpressions(expression_string)
+    assert len(subexpressions) == 3
