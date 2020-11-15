924b93e77116e790938909d3e19f58ef5cd98319
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-11-15 17:49:15 +0100

    test: parametrized is operation sign test

--- function_analyzer/tests/unitary/domain/test_sign.py
+++ function_analyzer/tests/unitary/domain/test_sign.py
@@ -8,9 +8,6 @@ def test_is_sign_when_sign_given(sign):
     assert Sign.is_sign(sign) is True
 
 
-# TODO parametrize after adding more signs
-# @pytest.mark.parametrize("sign_position, expression_string", [(0, '-1'), (2, '1*-1')])
-def test_is_operation_sign_returns_false_for_negative_operand_sign():
-    sign_position = 0
-    expression_string = "-1"
+@pytest.mark.parametrize("sign_position, expression_string", [(0, '-1'), (2, '1*-1')])
+def test_is_operation_sign_returns_false_for_negative_operand_sign(sign_position, expression_string):
     assert Sign.is_operation_sign(sign_position, expression_string) is False
