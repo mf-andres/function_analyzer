6d41d0d9be0d21d72c31b17bcb3488d8523ecbcc
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2020-11-15 17:51:04 +0100

    refactor: operation sorter

--- function_analyzer/infrastracture/operation_sorter.py
+++ function_analyzer/infrastracture/operation_sorter.py
@@ -1,9 +1,9 @@
 from function_analyzer.domain.operation.operation import Operation
 
 
-# TODO make domain services and just unit test the use cases
 class OperationSorter:
-    def sort_by_priority(self, operations: [Operation]):
+    @staticmethod
+    def sort_by_priority(operations: [Operation]):
         operations = sorted(
             operations, key=lambda x: (x.sign_priority, x.sign_position)
         )
